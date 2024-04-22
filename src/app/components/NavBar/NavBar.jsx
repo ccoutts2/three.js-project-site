bd9486ea680b1201cd2f3569dce5298c74722b65
Chris Coutts <chris.dcoutts@gmail.com>
2024-04-22 09:39:17 +0100

    Add overlay animation

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -1,7 +1,9 @@
 "use client";
 
 import styles from "./NavBar.module.scss";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import Link from "next/link";
 
 const links = [
@@ -21,40 +23,84 @@ const links = [
 
 const NavBar = () => {
   const container = useRef();
+  const overlay = useRef();
+  const linkHolders = useRef([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const tl = useRef();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  useGSAP(
+    () => {
+      let ctx = gsap.context(() => {
+        gsap.set(linkHolders.current, { y: 75 });
+
+        tl.current = gsap
+          .timeline({ paused: true })
+          .to(overlay.current, {
+            duration: 1.25,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease: "power4.inOut",
+          })
+          .to(linkHolders.current, {
+            y: 0,
+            duration: 1.25,
+            stagger: 0.1,
+            ease: "power4.inOut",
+            delay: -0.75,
+          });
+      });
+
+      return () => ctx.revert();
+    },
+    { scope: container }
+  );
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      tl.current.play();
+    } else {
+      tl.current.reverse();
+    }
+  }, [isMenuOpen]);
+
+  console.log(isMenuOpen);
+
   return (
-    <nav className={styles.navContainer} ref={container}>
-      <div className={styles.navBar}>
-        <div className={styles.logo}>
+    <nav className={styles.menuContainer} ref={container}>
+      <div className={styles.menuBar}>
+        <div className={styles.menuLogo}>
           <Link href="/">chris coutts</Link>
         </div>
-        <div className={styles.navOpen} onClick={toggleMenu}>
+        <div className={styles.menuOpen} onClick={toggleMenu}>
           <p>menu</p>
         </div>
       </div>
-      <div className={styles.navOverlay}>
-        <div className={styles.overlayBar}>
-          <div className={styles.logo}>
+      <div ref={overlay} className={styles.menuOverlay}>
+        <div className={styles.menuOverlayBar}>
+          <div className={styles.menuLogo}>
             <Link href="/">chris coutts</Link>
           </div>
+          <div className={styles.menuClose} onClick={toggleMenu}>
+            <p>close</p>
+          </div>
         </div>
-        <div className={styles.close} onClick={toggleMenu}>
-          <p>close</p>
-        </div>
-        <div className={styles.closeIcon}>
+
+        <div className={styles.menuCloseIcon}>
           <p>&#x2715;</p>
         </div>
-        <div className={styles.copy}>
+        <div className={styles.menuCopy}>
           <div className={styles.menuLinks}>
             {links.map((link, index) => (
-              <div key={index} className={styles.item}>
-                <div className={styles.itemHolder} onClick={toggleMenu}>
-                  <Link href={link.path} className={styles.link}>
+              <div key={index} className={styles.menuLinkItem}>
+                <div
+                  ref={(el) => (linkHolders.current[index] = el)}
+                  className={styles.menuLinkItemHolder}
+                  onClick={toggleMenu}>
+                  <Link href={link.path} className={styles.menuLink}>
                     {link.label}
                   </Link>
                 </div>
@@ -62,19 +108,16 @@ const NavBar = () => {
             ))}
           </div>
           <div className={styles.menuInfo}>
-            <div className={styles.infoCol}>
+            <div className={styles.menuInfoCol}>
               <Link href="/">X &#8599;</Link>
               <Link href="/">portfolio</Link>
               <Link href="/">github</Link>
               <Link href="/">linkedin</Link>
             </div>
-            <div className={styles.infoCol}>
+            <div className={styles.menuInfoCol}>
               <Link href="mailto:chris.dcoutts@gmail.com">email</Link>
             </div>
           </div>
-        </div>
-        <div className={styles.preview}>
-          <p>view showreel</p>
         </div>
       </div>
     </nav>
