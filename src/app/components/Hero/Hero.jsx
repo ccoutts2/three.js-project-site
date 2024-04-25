458f50ea6340c969860571ac9dd8e51a7eb89573
Chris Coutts <chris.dcoutts@gmail.com>
2024-04-25 10:00:40 +0100

    WIP - add animation timeline

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,4 +1,6 @@
 import styles from "./Hero.module.scss";
+import Image from "next/image";
+import Picture1 from "../../../../public/assets/images/nice2.JPG";
 
 import React, { useRef, useEffect } from "react";
 import { useGSAP } from "@gsap/react";
@@ -16,12 +18,21 @@ const Hero = () => {
   const letterWrapper2 = useRef([]);
   const headerItem1 = useRef();
   const headerItem2 = useRef();
+  const mainItem = useRef();
+  const sideLeft = useRef([]);
+  const sideRight = useRef([]);
+  const copyWrapperLeft = useRef([]);
+  const copyWrapperRight = useRef([]);
+  const copyWrapperMain = useRef();
 
   useGSAP(
     () => {
       let ctx = gsap.context(() => {
         gsap.set(letterWrapper1.current, { y: 400 });
         gsap.set(letterWrapper2.current, { y: 400 });
+        gsap.set(copyWrapperLeft.current, { y: 50 });
+        gsap.set(copyWrapperRight.current, { y: 50 });
+        gsap.set(copyWrapperMain.current, { y: 50 });
 
         tl.current = gsap
           .timeline({ paused: true, delay: 0.5 })
@@ -48,29 +59,81 @@ const Hero = () => {
             {
               duration: 1,
               ease: "power3.out",
-              right: "14vw",
+              right: "12vw",
               padding: "0.5rem",
             },
             "<"
           )
-          .to(headerItem1.current, {
-            left: 0,
+          .to(
+            mainItem.current,
+            {
+              duration: 1,
+              ease: "power3.out",
+              clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+            },
+            "<"
+          )
+          .to(mainItem.current, {
+            duration: 1,
+            ease: "power3.out",
             scale: 1,
-            bottom: "-2em",
           })
           .to(
-            headerItem2.current,
+            headerItem1.current,
             {
-              right: 0,
+              duration: 1,
+              ease: "power3.out",
+              left: 0,
               scale: 1,
-              bottom: "-2em",
+              bottom: "-15em",
             },
             "<"
           )
           .to(
-            container.current,
+            headerItem2.current,
+            { duration: 1, ease: "power3.out", right: 0, scale: 1, bottom: "-15em" },
+            "<"
+          )
+          .to(sideLeft.current, {
+            duration: 1,
+            ease: "power3.inOut",
+            clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+            stagger: 0.1,
+          })
+          .to(
+            sideRight.current,
             {
-              bottom: "2em",
+              duration: 1,
+              delay: 0.5,
+              ease: "power3.inOut",
+              clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+              stagger: 0.1,
+            },
+            "<"
+          )
+          .to(copyWrapperLeft.current, {
+            duration: 1,
+            ease: "power3.out",
+            y: 0,
+            stagger: 0.05,
+          })
+          .to(
+            copyWrapperRight.current,
+            {
+              duration: 1,
+              ease: "power3.out",
+              y: 0,
+              stagger: 0.05,
+            },
+            "<"
+          )
+          .to(
+            copyWrapperMain.current,
+            {
+              y: 0,
+              duration: 1,
+              ease: "power3.out",
+              stagger: 0.05,
             },
             "<"
           );
@@ -85,8 +148,187 @@ const Hero = () => {
     tl.current.play();
   }, []);
 
+  const projectsLeft = [
+    {
+      title: "cat",
+      href: "/projects/cat",
+      src: "/assets/images/nice2.JPG",
+      id: 1,
+    },
+    {
+      title: "dog",
+      href: "/projects/cat",
+      src: "/assets/images/nice3.JPG",
+      id: 2,
+    },
+    {
+      title: "ball",
+      href: "/projects/cat",
+      src: "/assets/images/nice4.JPG",
+      id: 3,
+    },
+  ];
+
+  const projectsRight = [
+    {
+      title: "turtle",
+      href: "/projects/cat",
+      src: "/assets/images/nice6.JPG",
+      id: 5,
+    },
+    {
+      title: "mouse",
+      href: "/projects/cat",
+      src: "/assets/images/nice7.JPG",
+      id: 6,
+    },
+    {
+      title: "square",
+      href: "/projects/cat",
+      src: "/assets/images/nice6.JPG",
+      id: 7,
+    },
+  ];
+
   return (
     <>
+      <section className={styles.projectSection}>
+        <div className={styles.container}>
+          <div className={styles.items}>
+            <div className={styles.itemsCol}>
+              {projectsLeft.map((project, index) => (
+                <div
+                  key={project.id}
+                  className={`${styles.item} ${styles.itemSide}`}>
+                  <div className={styles.itemCopy}>
+                    <div className={styles.itemCopyWrapper}>
+                      <p ref={(e) => (copyWrapperLeft.current[index] = e)}>
+                        {project.title}
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    ref={(e) => (sideLeft.current[index] = e)}
+                    className={styles.itemImg}>
+                    <Image fill={true} src={project.src} />
+                  </div>
+                </div>
+              ))}
+              {/* <div className={`${styles.item} ${styles.itemSide}`}>
+                <div className={styles.itemCopy}>
+                  <div ref={copyWrapperP1} className={styles.itemCopyWrapper}>
+                    <p>Cat</p>
+                  </div>
+                  <div ref={copyWrapperP2} className={styles.itemCopyWrapper}>
+                    <p>Lorem</p>
+                  </div>
+                </div>
+                <div ref={sideItem1} className={styles.itemImg}>
+                  <Image fill={true} src={Picture1} />
+                </div>
+              </div>
+              <div className={`${styles.item} ${styles.itemSide}`}>
+                <div className={styles.itemCopy}>
+                  <div ref={copyWrapperP3} className={styles.itemCopyWrapper}>
+                    <p>Dog</p>
+                  </div>
+                  <div ref={copyWrapperP4} className={styles.itemCopyWrapper}>
+                    <p>Lorem</p>
+                  </div>
+                </div>
+                <div ref={sideItem2} className={styles.itemImg}>
+                  <Image fill={true} src={Picture1} />
+                </div>
+              </div>
+              <div className={`${styles.item} ${styles.itemSide}`}>
+                <div className={styles.itemCopy}>
+                  <div ref={copyWrapperP5} className={styles.itemCopyWrapper}>
+                    <p>Fish</p>
+                  </div>
+                  <div ref={copyWrapperP6} className={styles.itemCopyWrapper}>
+                    <p>Lorem</p>
+                  </div>
+                </div>
+                <div ref={sideItem3} className={styles.itemImg}>
+                  <Image fill={true} src={Picture1} />
+                </div>
+              </div> */}
+            </div>
+            <div className={styles.itemsCol}>
+              <div className={`${styles.item} ${styles.itemMain}`}>
+                <div className={styles.itemCopy}>
+                  <div className={styles.itemCopyWrapper}>
+                    <p ref={copyWrapperMain}>Bee</p>
+                  </div>
+                </div>
+                <div className={styles.itemImg}>
+                  <Image ref={mainItem} fill={true} src={Picture1} />
+                </div>
+              </div>
+            </div>
+            <div className={styles.itemsCol}>
+              {projectsRight.map((project, index) => (
+                <div
+                  key={project.id}
+                  className={`${styles.item} ${styles.itemSide}`}>
+                  <div className={styles.itemCopy}>
+                    <div className={styles.itemCopyWrapper}>
+                      <p ref={(e) => (copyWrapperRight.current[index] = e)}>
+                        {project.title}
+                      </p>
+                    </div>
+                  </div>
+                  <div
+                    ref={(element) => (sideRight.current[index] = element)}
+                    className={styles.itemImg}>
+                    <Image fill={true} src={project.src} />
+                  </div>
+                </div>
+              ))}
+
+              {/* <div className={`${styles.item} ${styles.itemSide}`}>
+                <div className={styles.itemCopy}>
+                  <div ref={copyWrapperP9} className={styles.itemCopyWrapper}>
+                    <p>Hum</p>
+                  </div>
+                  <div ref={copyWrapperP10} className={styles.itemCopyWrapper}>
+                    <p>Lorem</p>
+                  </div>
+                </div>
+                <div ref={sideItem4} className={styles.itemImg}>
+                  <Image fill={true} src={Picture1} />
+                </div>
+              </div>
+              <div className={`${styles.item} ${styles.itemSide}`}>
+                <div className={styles.itemCopy}>
+                  <div ref={copyWrapperP11} className={styles.itemCopyWrapper}>
+                    <p>Dee</p>
+                  </div>
+                  <div ref={copyWrapperP12} className={styles.itemCopyWrapper}>
+                    <p>Lorem</p>
+                  </div>
+                </div>
+                <div ref={sideItem5} className={styles.itemImg}>
+                  <Image fill={true} src={Picture1} />
+                </div>
+              </div>
+              <div className={`${styles.item} ${styles.itemSide}`}>
+                <div className={styles.itemCopy}>
+                  <div ref={copyWrapperP13} className={styles.itemCopyWrapper}>
+                    <p>Nice</p>
+                  </div>
+                  <div ref={copyWrapperP14} className={styles.itemCopyWrapper}>
+                    <p>Lorem</p>
+                  </div>
+                </div>
+                <div ref={sideItem6} className={styles.itemImg}>
+                  <Image fill={true} src={Picture1} />
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </section>
       <div ref={container} className={styles.header}>
         <div
           ref={headerItem1}
@@ -118,5 +360,30 @@ const Hero = () => {
     </>
   );
 };
+
+// const Projects = ({ item, projects }) => {
+//   return (
+//     <div ref={item} className={styles.container}>
+//       <div className={styles.items}>
+//         {projects.map((project) => (
+//           <div key={project.id} className={styles.itemsCol}>
+//             <div
+//               id={`project-${project.id}`}
+//               className={`${styles.item} ${styles.itemSide}`}>
+//               <div className={styles.copy}>
+//                 <div className={styles.itemCopyWrapper}>
+//                   <p>{project.title}</p>
+//                 </div>
+//               </div>
+//               <div className={styles.itemImg}>
+//                 <Image fill={true} src={project.src} />
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Hero;
